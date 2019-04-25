ec5e7d917c1b7b30a1aaff9c0f491f0c56ca27c8
Marcelo Novaes <mnovaes@gmail.com>
2019-04-25 07:28:07 +0200

    Update python/clock to pass new test
    Signed-off-by: Marcelo Novaes <mnovaes@gmail.com>

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -11,14 +11,16 @@ MINS_IN_HOUR = 60
 
 class Clock(object):
 
-    def __init__(self, hours, minutes, add_min=0):
-        total_min = hours * MINS_IN_HOUR + minutes + add_min
+    def __init__(self, hours, minutes):
+        total_min = hours * MINS_IN_HOUR + minutes
         self.hours = (total_min // MINS_IN_HOUR) % HOURS_IN_DAY
         self.minutes = (total_min - self.hours * MINS_IN_HOUR) % MINS_IN_HOUR
 
-    def add(self, minutes):
-        self.__init__(self.hours, self.minutes, add_min=minutes)
-        return self
+    def __add__(self, minutes):
+        return self.__class__(self.hours, self.minutes + minutes)
+
+    def __sub__(self, minutes):
+        return self.__class__(self.hours, self.minutes - minutes)
 
     def __eq__(self, comparison):
         return self.__repr__() == comparison.__repr__()
