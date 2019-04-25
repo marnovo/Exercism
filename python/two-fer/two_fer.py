2580dbc0deccf886ec4975dc10396c20d5943b97
Marcelo Novaes <mnovaes@gmail.com>
2019-04-25 07:47:09 +0200

    Add Python #2 two-fer
    Signed-off-by: Marcelo Novaes <mnovaes@gmail.com>

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,2 +1,14 @@
-def two_fer(name):
+#
+# Exercism.io
+# Python track
+# Problem #3 - Two Fer
+# github.com/marnovo/Exercism
+#
+
+
+def two_fer(name=None):
+    if name:
+        return f"One for {name}, one for me."
+    else:
+        return("One for you, one for me.")
     pass
